fc1d625fac4046d07e2e9a2bb062074c6e41b0f8
alexandrudaniel3 <alexandrudanielbortos@gmail.com>
2023-09-26 23:10:18 +0300

    added logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className='home-page'>
             <div className='header' style={{height: 'fit-content', backgroundColor: '#5aa9e6', padding: "5px"}}>
-                <h1 style={{color: "white", margin: 0, textAlign: "center"}}>Title</h1>
+                <h1 style={{color: "white", margin: 0, textAlign: "center", fontFamily: "Courier Prime, monospace"}}>CodeLight</h1>
             </div>
             <Box />
         </div>
